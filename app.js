e3d50b41af4de09b2c8f8270487dcdb095010540
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-01-02 19:12:49 +0200

    added port

--- app.js
+++ app.js
@@ -38,4 +38,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
 module.exports = app;
