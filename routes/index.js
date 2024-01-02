7ea3f1c642f0734123c0bde5e7dab5e94181f3fc
Kostas Paplauskas <paplauskaskostas@gmail.com>
2024-01-02 19:14:47 +0200

    added router.get for the two pages and sample messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Mini Message Board",
+    messages: messages,
+  });
 });
+
+router.get('/new', function(req, res) {
+  res.render("form")
+})
 
 module.exports = router;
